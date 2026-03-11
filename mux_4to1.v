c30fb5fe8e7b32950147fdff81c5e317e04f7ff8
Michelle Joo <michellejoo@u.boisestate.edu>
2026-03-11 14:09:46 -0600

    Create mux_4to1.v

---
+++ mux_4to1.v
@@ -0,0 +1,12 @@
+module mux_4to1(
+  input [3:0] CEO,
+  input [3:0] You,
+  input [3:0] Fred,
+  input [3:0] Jill,
+  input [3:0] sel,
+  input Enable,
+  output [3:0] Y
+);
+  assign Y = Enable ? (sel == 2'b00 ? CEO : sel == 2'b01 ? You : sel == 2'b10 ? Fred : sel == 2'b11 ? Jill: 0) : 0;
+
+endmodule
